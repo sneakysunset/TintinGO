2b78b166fcd92a8b0df6470199d48135aa7a72f9
Sneakysunset <andrelauzier@gmail.com>
2023-11-06 19:29:53 +0100

    Base Script

--- Source/TintinGO/MainGameMode.h
+++ Source/TintinGO/MainGameMode.h
@@ -1,17 +1,20 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Tile.h"
 #include "MainGameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TINTINGO_API AMainGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public :
+	UPROPERTY(EditAnywhere)
+	static int32 _rows;
+	UPROPERTY(EditAnywhere)
+	static int32 _columns;
+	UPROPERTY(EditAnywhere)
+	static float _tileWidth;
+	static TArray<TArray<Tile>> _tileGrid;
 };
