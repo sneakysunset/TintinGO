3e0d67f6269a8410b5f319d93115eb292af1e67b
flo-viseux <frinola.games@gmail.com>
2023-11-17 20:29:21 +0100

    création TileCharacter_Enemy + instanciation de l'ensemble des PlacableBodies grâce à l'interface et à la liste PlacableBodies regroupant l'ensemble des items/enemies/personnage

---
+++ Source/TintinGO/TileCharacter_Enemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TileCharacter.h"
+#include "TileCharacter_Enemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINTINGO_API ATileCharacter_Enemy : public ATileCharacter
+{
+	GENERATED_BODY()
+	
+};
